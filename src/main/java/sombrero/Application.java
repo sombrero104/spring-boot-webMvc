f739533f4f326372bec51e8a34fbc520e4939f84
sombrero104 <sombrero.khy@gmail.com>
2019-06-25 15:42:29 +0900

    .

--- src/main/java/sombrero/Application.java
+++ src/main/java/sombrero/Application.java
@@ -10,4 +10,11 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    /**
+     * welcome page
+     * 에러 -> 에러핸들러가 만들어주는 뷰 표시.
+     *
+     * / 루트 요청 시 -> 정적페이지(기본 4가지 리소스 위치 중 index.html 파일 찾음.), 동적페이지(핸들링 엔진)
+     *
+     */
 }
